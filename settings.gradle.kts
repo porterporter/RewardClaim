2c121a26271192a775149f1aa22056077b423262
porter milton <57007161+porterporter@users.noreply.github.com>
2024-12-28 21:39:54 -0600

    [actions skip] broken...

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,31 @@
-@file:Suppress("PropertyName")
+import groovy.lang.MissingPropertyException
 
 pluginManagement {
     repositories {
+        // Repositories
+        maven("https://maven.deftu.dev/releases")
+        maven("https://maven.fabricmc.net")
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.minecraftforge.net")
+        maven("https://repo.essential.gg/repository/maven-public")
+        maven("https://server.bbkr.space/artifactory/libs-release/")
+        maven("https://jitpack.io/")
+
+        // Snapshots
+        maven("https://maven.deftu.dev/snapshots")
+        mavenLocal()
+
+        // Default repositories
         gradlePluginPortal()
         mavenCentral()
-        maven("https://repo.polyfrost.org/releases") // Adds the Polyfrost maven repository to get Polyfrost Gradle Toolkit
     }
+
     plugins {
-        val pgtVersion = "0.6.5" // Sets the default versions for Polyfrost Gradle Toolkit
-        id("org.polyfrost.multi-version.root") version pgtVersion
+        kotlin("jvm") version("2.0.0")
+        id("dev.deftu.gradle.multiversion-root") version("2.12.0")
     }
 }
+
 
 val mod_name: String by settings
 
